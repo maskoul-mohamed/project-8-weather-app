f40c60710b5a2293481156df018d31b7e97e94c4
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-01-14 12:45:48 +0000

    Add show list

--- src/main.js
+++ src/main.js
@@ -1,24 +1,11 @@
 var city;
 
-
 var key = 'c2d6f6f974cc0761aeacc70ce99d3e25';
-
+var weatherManager = new WeatherManager()
 
 function onSearch() {
     city = document.getElementById("cityInput").value
-    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
-    fetch(url)
-        .then(response => {
-            // handle the response
-            return response.json()
-        }).then(json => {
-            var city = json.name
-            var weather = json.weather[0].main
-            console.log(city)
-            console.log(weather)
-        })
-        .catch(error => {
-            // handle the error
-            console.log(error)
-        });
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
+    weatherManager.fetchForecast(url)
+    document.getElementById("cityInput").value = ""
 }
